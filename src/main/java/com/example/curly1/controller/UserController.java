1ebd7c55ab621f2432012fe1a72d47deda048a3f
smolarekman <smolarekman@gmail.com>
2019-02-01 22:29:39 +0100

    user controller

--- src/main/java/com/example/curly1/controller/UserController.java
+++ src/main/java/com/example/curly1/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.curly1.exception.ResourceNotFoundException;
 import com.example.curly1.model.UserModel;
 import com.example.curly1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,18 +18,46 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping("/getAllUsers")
-    public List<UserModel> getAllUsers(){
+    public List<UserModel> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/getUserById/{id}")
-    public UserModel getUser(@PathVariable(value="id") Long userId){
+    public UserModel getUser(@PathVariable(value = "id") Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(()->new ResourceNotFoundException("User "+ "id :"+userId));
+                .orElseThrow(() -> new ResourceNotFoundException("User " + "id :" + userId));
     }
 
-    @PostMapping("/addNewUser")     /**     PRZY LOGOWANIU ZMIENIC      **/
-    public UserModel addNewUser(@Valid @RequestBody UserModel userModel){
+    @PostMapping("/addNewUser")
+    /**     PRZY LOGOWANIU ZMIENIC      **/
+    public UserModel addNewUser(@Valid @RequestBody UserModel userModel) {
         return userRepository.save(userModel);
     }
+
+    @PutMapping("/editUser/{id}")
+    public UserModel updateUser(@PathVariable(value = "id") Long userId,
+                                @Valid @RequestBody UserModel userModel) {
+        UserModel newUser=userRepository.findById(userId)
+                .orElseThrow(()->new ResourceNotFoundException("User"+"id"+userId));
+
+        newUser.setUserName(userModel.getUserName());
+        newUser.setUserLastName(userModel.getUserLastName());
+        newUser.setUserEmail(userModel.getUserEmail());
+        newUser.setUserPassword(userModel.getUserPassword());
+
+        return userRepository.save(newUser);
+    }
+
+    @DeleteMapping("/deleteUser/{userId}")
+    public ResponseEntity <?> deleteUser(@PathVariable(value = "userId") Long userId)
+    {
+        UserModel userModel=userRepository.findById(userId)
+                .orElseThrow(()->new ResourceNotFoundException("User"+"id"+userId));
+
+        userRepository.delete(userModel);
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
